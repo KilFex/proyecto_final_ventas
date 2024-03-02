2a2744e38663ad64d11f5f2674bb1cb82a8c1f84
KilFex <elivan.1251996@gmail.com>
2024-03-02 01:58:06 -0500

    Actualizacion

---
+++ src/Arreglos/ArregloClientes.java
@@ -0,0 +1,17 @@
+package Arreglos;
+
+import java.util.ArrayList;
+
+import Clases.Cliente;
+
+public class ArregloClientes {
+	
+	private ArrayList<Cliente> arrClientes;
+	
+	public ArregloClientes() {
+		arrClientes = new ArrayList<Cliente>(); 
+	}
+	
+	
+	
+}
